0b7a31b4a5f2875d5bb8481022e38e54c0c596bb
Almas Banu <almasbanuu234@gmail.com>
2026-06-26 16:14:46 +0530

    Remove unused code from conditions

--- 04-conditions/exercise-08-grade-evaluator.js
+++ 04-conditions/exercise-08-grade-evaluator.js
@@ -8,26 +8,3 @@ if (marks >= 90){
 }else{
     console.log("Fail");
 }
-
-
-
-// mini challenge
-let passwordLength = 10;
-let hasUppercase = true;
-let hasNumber = true;
-if (passwordLength >= 8 && hasUppercase && hasNumber ){
-    console.log("Strong Password");
-}else{
-    console.log("Weak Password");
-}
-
-// mentor prediction challenge
-
-let age = 20;
-if (age >= 18){
-    console.log("Adult");
-}else if(age >= 13){
-    console.log("Teenager")
-}else{
-    console.log("Child");
-}
