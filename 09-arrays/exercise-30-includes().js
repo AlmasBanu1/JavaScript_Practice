c0646027a5cf5c63c45130de0e133f300ac21ffb
Almas Banu <almasbanuu234@gmail.com>
2026-07-22 18:29:19 +0530

    Add practice for Array.includes() method

---
+++ 09-arrays/exercise-30-includes().js
@@ -0,0 +1,15 @@
+let cities = [
+
+    "Delhi",
+    "Mumbai",
+    "Bangalore",
+    "Chennai"
+
+];
+
+let cityFound = cities.includes("Bangalore");
+if(cityFound){
+    console.log("City Found");
+}else{
+    console.log("City Not Found");
+}
