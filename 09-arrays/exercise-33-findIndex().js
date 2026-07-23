b797cdbca06c10f625908e500afcb04a76fbb21a
Almas Banu <almasbanuu234@gmail.com>
2026-07-23 09:52:41 +0530

    Add practice for Array.findIndex() method

---
+++ 09-arrays/exercise-33-findIndex().js
@@ -0,0 +1,20 @@
+let numbers = [
+
+    5,
+    10,
+    15,
+    20
+
+];
+
+let answer = numbers.findIndex(
+
+    function(number) {
+
+        return number > 12;
+
+    }
+
+);
+
+console.log(answer);
